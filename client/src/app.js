ae3e043dc8ece59d8809f0a7119c77f574008e64
Don McCurdy <don.r.mccurdy@gmail.com>
2016-01-26 00:17:31 -0500

    Better env management of public host/port

--- client/src/app.js
+++ client/src/app.js
@@ -6,8 +6,8 @@ angular.module('proxyControlsApp', [
     require('angular-touch')
   ])
   .constant('SERVER', {
-    HOST: process.env.npm_package_config_host,
-    PORT: process.env.npm_package_config_port
+    HOST: process.env.PUBLIC_HOST,
+    PORT: process.env.PUBLIC_PORT
   });
 
 require('./controllers');
