c5c0f7d885a00ef427298107f342dcdc5bd6e8fe
Don McCurdy <don.r.mccurdy@gmail.com>
2016-01-27 21:49:20 -0500

    Add navigation sidebar.

--- client/controllers/nav-ctrl.js
+++ client/controllers/nav-ctrl.js
@@ -1,5 +1,20 @@
-module.exports = function ($scope, $location) {
-	$scope.isActive = function (path) {
-		return path === $location.path();
-	};
+module.exports = function ($scope, $document, $element, $location) {
+	
+	// Navigation drawer state.
+	$scope.navOpen = false;
+
+	// Check if given path is active.
+	$scope.isActive = function (path) { return path === $location.path(); };
+
+	// Close navigation if page changes.
+	$scope.$on('$locationChangeStart', function() {
+	  $scope.navOpen = false;
+	});
+
+	// Close navigation if user clicks outside nav.
+	$element.on('click', function (e) { e.stopPropagation(); });
+	$document.on('click', function () {
+		$scope.$apply(function () { $scope.navOpen = false; });
+	});
+
 };
