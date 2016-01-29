4c13fdb7261ba928884bcded2be40e71897941de
Don McCurdy <don.r.mccurdy@gmail.com>
2016-01-28 22:57:00 -0500

    Nav fixes for Safari. Bleh.

--- client/controllers/nav-ctrl.js
+++ client/controllers/nav-ctrl.js
@@ -1,20 +1,35 @@
-module.exports = function ($scope, $document, $element, $location) {
-	
-	// Navigation drawer state.
-	$scope.navOpen = false;
+module.exports = function ($scope, $document, $window, $element, $location) {
+  
+  // Navigation drawer state.
+  $scope.navOpen = false;
 
-	// Check if given path is active.
-	$scope.isActive = function (path) { return path === $location.path(); };
+  // Check if given path is active.
+  $scope.isActive = function (path) { return path === $location.path(); };
 
-	// Close navigation if page changes.
-	$scope.$on('$locationChangeStart', function() {
-	  $scope.navOpen = false;
-	});
+  // Close navigation if page changes.
+  $scope.$on('$locationChangeStart', function() {
+    $window.scroll(0, 0);
+    $scope.navOpen = false;
+  });
 
-	// Close navigation if user clicks outside nav.
-	$element.on('click', function (e) { e.stopPropagation(); });
-	$document.on('click', function () {
-		$scope.$apply(function () { $scope.navOpen = false; });
-	});
+  var closeNav = function (e) {
+    if ($scope.navOpen) {
+      $scope.$apply(function () { $scope.navOpen = false; });
+      e.preventDefault();
+    }
+  };
+
+  var isolateNav = function (e) { e.stopPropagation(); };
+
+  // Close navigation if user clicks outside nav.
+  $element.on('click', isolateNav);
+  $element.on('touchstart', isolateNav);
+  $document.on('touchstart', closeNav);
+  $document.on('click', closeNav);
+
+  $scope.$on('$destroy', function () {
+    $document.off('touchstart', closeNav);
+    $document.off('click', closeNav);
+  });
 
 };
