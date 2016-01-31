85a4ff4644e0f2eec7027be19901313e9f0a8c1a
Don McCurdy <don.r.mccurdy@gmail.com>
2016-01-31 15:50:45 -0500

    Choose primary home button based on device.

--- client/controllers/home-ctrl.js
+++ client/controllers/home-ctrl.js
@@ -1,3 +1,4 @@
-module.exports = function ($scope, RESOURCES) {
+module.exports = function ($scope, $window, RESOURCES) {
   $scope.demoUrl = RESOURCES.DEMO_URL;
+  $scope.isMobile = $window.matchMedia('(max-width: 550px)').matches;
 };
