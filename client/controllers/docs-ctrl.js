cb2c1f9d3d62abf405e38786de11d510cbf0cea1
Don McCurdy <don.r.mccurdy@gmail.com>
2016-01-30 19:37:29 -0500

    Autoscroll to hash on load.

--- client/controllers/docs-ctrl.js
+++ client/controllers/docs-ctrl.js
@@ -2,6 +2,7 @@ module.exports = function ($scope, $location, $anchorScroll) {
 
 	var originalOffset = $anchorScroll.yOffset;
 	$anchorScroll.yOffset = 100;
+	$anchorScroll();
 
   $scope.goHash = function (anchor) {
   	$location.hash(anchor);
