151094c97d4df08d9e8c4195276722a5e45087ae
Don McCurdy <don.r.mccurdy@gmail.com>
2016-02-02 20:01:58 -0500

    fix gap in background on spring anti-scroll / mobile safari

--- client/controllers/home-ctrl.js
+++ client/controllers/home-ctrl.js
@@ -1,4 +1,9 @@
 module.exports = function ($scope, $window, RESOURCES) {
   $scope.demoUrl = RESOURCES.DEMO_URL;
   $scope.isMobile = $window.matchMedia('(max-width: 550px)').matches;
+
+  $window.document.body.classList.add('bg-dark');
+  $scope.$on('$destroy', function () {
+		$window.document.body.classList.remove('bg-dark');  	
+  });
 };
